e66e400a5b036e014d57d526d9f0fb3e6d1d8e5b
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2018-11-02 00:02:45 -0400

    Add the templates for IterativeFactorialComputation and RecursiveFactorialComputation.

---
+++ src/main/java/practicalsix/computation/IterativeFactorialComputation.java
@@ -0,0 +1,16 @@
+package practicalsix.computation;
+
+public class IterativeFactorialComputation extends FactorialComputation {
+
+  // TODO: Add the required constructor
+
+  // TODO: Add the compute method with an appropriate JavaDoc
+
+  // REMEMBER: Please review previous labs and practicals for
+  // inspiration concerning how to complete these tasks
+
+  // NOTE: You can learn more about the factorial function by visiting the
+  // following web site:
+  // https://www.mathsisfun.com/numbers/factorial.html
+
+}
